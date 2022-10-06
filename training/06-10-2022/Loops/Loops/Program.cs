dcfa5e195a3e5dfc52aa868ae322a7c34ef50dc1
oki-imamovic <oki.imamovic@gmail.com>
2022-10-06 19:50:13 +0200

    While/For Loops Break and Continue

--- training/06-10-2022/Loops/Loops/Program.cs
+++ training/06-10-2022/Loops/Loops/Program.cs
@@ -6,7 +6,8 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            
+
+            /*
             //While Loop
 
             int i = 0;
@@ -26,6 +27,94 @@ namespace Loops
                 j++;
             }
             while (j < 5);
+
+
+            */
+
+            //Break and Continue While Loop
+
+            /*
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+                if (i == 4)
+                {
+                    break;
+                }
+            }
+            */
+
+
+            /*
+            int i = 0;
+            while (i < 10)
+            {
+                if (i == 4)
+                {
+                    i++;
+                    continue;
+                }
+                Console.WriteLine(i);
+                i++;
+            }
+            */
+
+
+
+            /*
+            //For Loop
+
+            for (int k = 0; k <= 5; k++)
+            {
+                Console.WriteLine(k);
+            }
+
+
+            for (int l = 0; l <= 10; l = l + 2)
+            {
+                Console.WriteLine(l);
+            }
+
+            //For Each
+
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach (string c in cars)
+            {
+                Console.WriteLine(c);
+            }
+            */
+
+
+            //C# Break
+
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+            */
+
+
+            //C# Continue
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i < 5)
+                {
+                    continue;
+                }
+                else 
+                {
+                    i += 4;
+                }
+                Console.WriteLine(i);
+            }
             
         }
     }
