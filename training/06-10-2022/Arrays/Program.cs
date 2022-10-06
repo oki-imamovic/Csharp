8d3cd81946ef3ab28a1d7acba364d885e977792c
oki-imamovic <oki.imamovic@gmail.com>
2022-10-06 19:57:11 +0200

    Simple Arrays

--- training/06-10-2022/Arrays/Program.cs
+++ training/06-10-2022/Arrays/Program.cs
@@ -6,7 +6,19 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            cars[0] = "Opel";
+
+            int[] nums = { 35, 15, 24, 32 };
+
+            Console.WriteLine(cars[2]);
+            Console.WriteLine(nums[2]);
+            Console.WriteLine(cars[0]);
+            Console.WriteLine(cars.Length);
+
+
+            string[] models = new string[4] { "Volvo", "BMW", "Ford", "Mazda" };
+            Console.WriteLine(models[2]);
         }
     }
 }
