33ebe76c03a26b6a41004e21c9da7eae94d655b4
oki-imamovic <oki.imamovic@gmail.com>
2022-10-06 20:06:55 +0200

    For and While Loop Trough Array

--- training/06-10-2022/Arrays/Program.cs
+++ training/06-10-2022/Arrays/Program.cs
@@ -19,6 +19,23 @@ namespace Arrays
 
             string[] models = new string[4] { "Volvo", "BMW", "Ford", "Mazda" };
             Console.WriteLine(models[2]);
+
+
+            Console.WriteLine("---WE DRAW THE LINE HERE---");
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+            Console.WriteLine("---WE DRAW THE LINE HERE---");
+
+            int j = 0;
+            while (j < cars.Length)
+            {
+                Console.WriteLine(cars[j]);
+                j++;
+            }
         }
     }
 }
