a9d5781e0c2f8f2f1e9a09c6d1505c633719bb4e
oki-imamovic <oki.imamovic@gmail.com>
2022-10-06 13:29:11 +0200

    Variables

---
+++ training/06-10-2022/Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 5;
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+            string myText = "Hello";
+
+            Console.WriteLine(myNum);
+            Console.WriteLine(myDoubleNum);
+            Console.WriteLine(myLetter);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myText);
+
+
+            int x = 24;
+            int y = 31;
+            int z = x + y;
+            Console.WriteLine(z);
+
+            string firstName = "Ohran";
+            string lastName = "Imamovic";
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName);
+        }
+    }
+}
