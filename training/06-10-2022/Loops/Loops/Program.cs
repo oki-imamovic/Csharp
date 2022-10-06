31538847bebf4fa80e2487defec3e35e77827e2f
oki-imamovic <oki.imamovic@gmail.com>
2022-10-06 19:34:15 +0200

    While and Do While Loops

---
+++ training/06-10-2022/Loops/Loops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //While Loop
+
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            
+
+            //Do While
+
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            while (j < 5);
+            
+        }
+    }
+}
