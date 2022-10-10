1a9093233c2ca157ad2fcf888929dc43d4cea608
oki-imamovic <oki.imamovic@gmail.com>
2022-10-10 14:43:31 +0200

    Get Set in Objects

---
+++ training/10-10-2022/Objects/Movies/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movies
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating;
+
+        //Changes on constructor from rating to => Rating we are calling the Method
+        public Movie(string aTitle, string aDircetor, string aRating) 
+        {
+            title = aTitle;
+            director = aDircetor;
+            Rating = aRating;
+        }
+
+        //Method
+        public string Rating 
+        {
+            //geter and seter
+            get { return rating; }
+            set 
+            {
+                // To write || use alt and 124 on num pad
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                {
+                    rating = value;
+                }
+                else 
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ training/10-10-2022/Objects/Movies/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "Dog");
+
+            Console.WriteLine(avengers.title);
+            Console.WriteLine(avengers.director);
+            Console.WriteLine(avengers.Rating);
+            Console.WriteLine();
+            Console.WriteLine(shrek.title);
+            Console.WriteLine(shrek.director);
+            Console.WriteLine(shrek.Rating);
+        }
+    }
+}
