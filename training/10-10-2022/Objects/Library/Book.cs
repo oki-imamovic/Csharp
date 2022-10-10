f891ae5d8fea7a036b2a693b01a197a54ae520ca
oki-imamovic <oki.imamovic@gmail.com>
2022-10-10 14:12:31 +0200

    Classes Objects and Constructor

---
+++ training/10-10-2022/Objects/Library/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    class Book
+    {
+        //Class atributes
+        public string title;
+        public string author;
+        public int pages;
+
+        //Method for Constructor
+        public Book(string aTitle, string aAuthor, int aPages) 
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+    }
+}
---
+++ training/10-10-2022/Objects/Library/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+
+            //Basic Class and Implementation
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 432;
+
+            Console.WriteLine(book1.author);
+
+            */
+
+            // Constructor
+            Book book1 = new Book("Harry Potter", "JK Rowling", 458);
+            Book book2 = new Book("Lord of the Rings", "Tolkein", 784);
+            book2.title = "The Hobbit";
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book2.title);
+            
+        }
+    }
+}
