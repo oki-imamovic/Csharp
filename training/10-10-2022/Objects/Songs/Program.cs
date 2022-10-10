d585840ea08d3a93523244eadf873e5f1adb05cf
oki-imamovic <oki.imamovic@gmail.com>
2022-10-10 15:01:52 +0200

    Static Class Atribute

---
+++ training/10-10-2022/Objects/Songs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Song.songCount);
+
+            Console.WriteLine();
+
+            Song holiday = new Song("Holiday", "Green Day", 320);
+
+            Console.WriteLine(holiday.title);
+            Console.WriteLine(holiday.artist);
+            Console.WriteLine(holiday.duration);
+            Console.WriteLine(Song.songCount);
+            
+            Console.WriteLine();
+
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 220);
+            Console.WriteLine(kashmir.title);
+            Console.WriteLine(kashmir.artist);
+            Console.WriteLine(kashmir.duration);
+            Console.WriteLine(Song.songCount);
+        }
+    }
+}
---
+++ training/10-10-2022/Objects/Songs/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Songs
+{
+    class Song
+    {
+        public string title;
+        public string artist;
+        public int duration;
+        //Static Class Atribute
+        public static int songCount = 0;
+
+        public Song(string aTitle, string aArtist, int aDuration) 
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            songCount++;
+        }
+
+    }
+}
