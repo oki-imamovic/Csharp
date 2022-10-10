fff04df1dd4bbb14645ace32b155ea47050bbd37
oki-imamovic <oki.imamovic@gmail.com>
2022-10-10 14:26:26 +0200

    Object Methods

---
+++ training/10-10-2022/Objects/Students/Students/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Object Methods
+            Student student1 = new Student("Ohran Imamovic", "IT", 3.2);
+            Student student2 = new Student("Eric Longshoot", "Medicine", 3.8);
+
+            Console.WriteLine(student1.HasHonor());
+            Console.WriteLine(student2.HasHonor());
+        }
+    }
+}
---
+++ training/10-10-2022/Objects/Students/Students/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Students
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student(string aName, string aMajor, double aGpa) 
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+        //Method
+        public bool HasHonor() 
+        {
+            if (gpa >= 3.5) 
+            {
+                return true;
+            }
+            return false;
+        }
+        
+    }
+}
